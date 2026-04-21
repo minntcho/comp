955647fb1ad90cd8637cdf7d4e4d9a25232e62ad
minntcho <limsh12069@gmail.com>
2026-04-21 15:10:40 +0900

    PR0: add judgment core placeholder

---
+++ comp/judgment/core.py
@@ -0,0 +1,10 @@
+"""Placeholder for the future judgment core.
+
+Planned contents:
+- SubjectRef
+- Fact
+- JudgmentState
+- append-only hazard / provenance primitives
+"""
+
+__all__: list[str] = []
