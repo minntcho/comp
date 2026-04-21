42590bb9a84b49ca7fba8128537ab63a5ceb7dc0
minntcho <limsh12069@gmail.com>
2026-04-21 15:10:13 +0900

    PR0: reserve judgment package

---
+++ comp/judgment/__init__.py
@@ -0,0 +1,9 @@
+"""Future judgment-core package.
+
+This namespace is reserved for the next architecture step:
+- fact/judgment core
+- compiled judgment program
+- fixpoint engine
+- frontier/arbitration
+- commit/receipts
+"""
