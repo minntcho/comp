0d2aa33d66d4fcb85208723897e00c078f9d863c
minntcho <limsh12069@gmail.com>
2026-04-21 16:31:17 +0900

    PR2: export bridge adapters from compat package

--- comp/compat/__init__.py
+++ comp/compat/__init__.py
@@ -1,4 +1,5 @@
 """Compatibility layer for existing top-level artifacts and specs."""
 
+from comp.compat.adapters import *
 from comp.compat.artifacts import *
 from comp.compat.compiled_spec import *
