f73916f077adf8b87bc6c480f97219a63250cc15
minntcho <limsh12069@gmail.com>
2026-04-21 17:19:58 +0900

    PR3: export public view helpers

--- comp/views/__init__.py
+++ comp/views/__init__.py
@@ -1,1 +1,13 @@
-"""Future projection/view package."""
+"""Projection and view helpers."""
+
+from comp.views.public import (
+    DEFAULT_PUBLIC_PROJECTION,
+    materialize_public_rows,
+    project_canonical_row,
+)
+
+__all__ = [
+    "DEFAULT_PUBLIC_PROJECTION",
+    "project_canonical_row",
+    "materialize_public_rows",
+]
